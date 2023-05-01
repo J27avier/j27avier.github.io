4e7dd770b4defeb2c8e15a83a842e979f5d79ebb
J27avier <salesortiz@gmail.com>
2023-05-01 01:24:47 -0600

    Added new projects

--- _site/js/freelancer.js
+++ _site/js/freelancer.js
@@ -9,7 +9,7 @@ $(function() {
     $('.page-scroll a').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
+            scrollTop: $($anchor.attr('href')).offset().top-60
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });
